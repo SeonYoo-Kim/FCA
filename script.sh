3322681c17e95a7b579ec93db048c4ddaf9400cb
theta <tjdn13@naver.com>
2024-08-22 19:05:13 +0900

    all

---
+++ script.sh
@@ -0,0 +1,5 @@
+python main.py dataset=mvtec fe=wide sc=fca image_size=original tile_size=[9,9]
+python main.py dataset=dtd fe=wide sc=fca image_size=original tile_size=[9,9]
+python main.py dataset=btad fe=wide sc=fca image_size=original tile_size=[9,9]
+python main.py dataset=wft fe=wide sc=fca image_size=original tile_size=[9,9]
+python main.py dataset=wfdd fe=wide sc=fca image_size=original tile_size=[9,9]
